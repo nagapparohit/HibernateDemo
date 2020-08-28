05726330c2adb1decab3a141e34af7b2b964f9d5
rohit nagappa <rnagappa35@gmail.com>
2020-08-29 00:16:59 +0530

    moved hibernate file

--- src/InitServlet.java
+++ src/InitServlet.java
@@ -9,13 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import com.ecommerce.HibernateUtil;
 
 /**
  * Servlet implementation class InitServlet
  */
-@WebServlet("/InitServlet")
+@WebServlet("/init")
 public class InitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -32,22 +34,25 @@ public class InitServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		try {
-			PrintWriter out = response.getWriter();
-			out.println("<html><body>");
+		
+			try {
+				PrintWriter out = response.getWriter();
+				out.println("<html><body>");
+				System.out.println("Before ruuning seesion facotry");
+				SessionFactory factory = HibernateUtil.getSessionFactory();
+				Session session = factory.openSession();
+				System.out.println("After running sesion facotry");
+				out.println("<h3>Hibernate session opened</h3>");
+				session.close();
+				out.println("<h3>Hibernate session closed</h3>");
+				
+				out.println("</body></html>");
+			} catch (HibernateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-			Session session = sessionFactory.openSession();
-			
-			out.println("<h3>Hibernate session opened</h3>");
-			session.close();
-			out.println("<h3>Hibernate session closed</h3>");
-			
-			out.println("</body></html>");
-			
-		} catch (Exception ex) {
-			throw ex;
-		}
+		
 		
 		
 	}
