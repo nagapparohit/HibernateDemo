93a76f36db75f4bf96978c7af79019fd2b4bdcb4
rohit nagappa <rnagappa35@gmail.com>
2020-08-28 23:17:02 +0530

    initial commit

---
+++ src/InitServlet.java
@@ -0,0 +1,63 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ * Servlet implementation class InitServlet
+ */
+@WebServlet("/InitServlet")
+public class InitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("<html><body>");
+			
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			Session session = sessionFactory.openSession();
+			
+			out.println("<h3>Hibernate session opened</h3>");
+			session.close();
+			out.println("<h3>Hibernate session closed</h3>");
+			
+			out.println("</body></html>");
+			
+		} catch (Exception ex) {
+			throw ex;
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
